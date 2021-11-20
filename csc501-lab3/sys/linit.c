72782c3be62a3461b0b6a6b92616520cee4e4350
srujan ponnur <sponnur@ncsu.edu>
2021-11-20 17:53:50 -0500

    fixed linit error

--- csc501-lab3/sys/linit.c
+++ csc501-lab3/sys/linit.c
@@ -12,10 +12,10 @@ void linit() {
 		locks[lock_index].lstatus = LFREE;
 		locks[lock_index].ltype = -1;
 		locks[lock_index].lqtail = 1 + (locks[lock_index].lqhead = newqueue_l());
-		for (proc_index = 0; proc_index < NPROC; prox_index++) {
+		for (proc_index = 0; proc_index < NPROC; proc_index++) {
 			locks[lock_index].proc_list[proc_index] = 0;
 		}
-		locks[lock_index].readerCount = 0;
+		locks[lock_index].reader_count = 0;
 		locks[lock_index].is_writer = 0;
 		locks[lock_index].is_deleted = 0; /* was this lock ever got deleted?*/
 	}
