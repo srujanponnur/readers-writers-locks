17da3ee4deff9e88c8d4d9320d761aa39fc80b81
srujan ponnur <sponnur@ncsu.edu>
2021-11-20 23:49:42 -0500

    added test6 and 7

--- csc501-lab3/sys/main.c
+++ csc501-lab3/sys/main.c
@@ -286,7 +286,7 @@ void writer6(char msg, int lck, int lprio)
     lock(lck, WRITE, lprio);
     output2[count2++] = msg;
     kprintf("  %c: acquired lock, sleep 3s\n", msg);
-    sleep(1);
+    sleep(5);
     output2[count2++] = msg;
     kprintf("  %c: to release lock\n", msg);
     releaseall(1, lck);
