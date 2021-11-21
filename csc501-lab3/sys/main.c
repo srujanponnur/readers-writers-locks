f24b415a8086b23f88a9ba2d37cbf38b30eb9a2a
srujan ponnur <sponnur@ncsu.edu>
2021-11-21 02:07:19 -0500

    added test8

--- csc501-lab3/sys/main.c
+++ csc501-lab3/sys/main.c
@@ -409,13 +409,13 @@ int main()
      * The provided results do not guarantee your correctness.
      * You need to read the PA2 instruction carefully.
      */
-    //test1();
-    //test2();
-    //test3();
-    //test4();
-    //test5();
-    //test6();
-    //test7();
+    test1();
+    test2();
+    test3();
+    test4();
+    test5();
+    test6();
+    test7();
     test8();
 
     /* The hook to shutdown QEMU for process-like execution of XINU.
