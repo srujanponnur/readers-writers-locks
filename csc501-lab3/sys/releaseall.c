5b039a7ab20a20bd2bebd26e923427dd310205a2
srujan ponnur <sponnur@ncsu.edu>
2021-11-26 00:41:29 -0500

    added debug statements

--- csc501-lab3/sys/releaseall.c
+++ csc501-lab3/sys/releaseall.c
@@ -64,7 +64,7 @@ int releaseall(int numlocks, int ldesc1) { // variable argument validation with 
 								temp = result;
 								waitprio = q[result].qkey;
 								while (q[temp].qkey == waitprio && temp!= q_head) {
-									kprintf("Coming here!\n")
+									kprintf("Coming here!\n");
 									r_waittime = ctr1000 - q[temp].added_at;
 									w_waittime = ctr1000 - q[writer].added_at;
 									//kprintf("The difference is: %d\n", (r_waittime - w_waittime));
